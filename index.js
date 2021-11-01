bf80135a402076fc0534775ba09619043218008c
Callum Gill <callumgill99@outlook.com>
2021-11-01 14:58:55 +0000

    ADDED JEST

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Hello world
+
+const favFilms = ['Air Force One', 'Taken', 'Happy Gilmore', 'Hanna Montana: The Movie']
+
+
+
+
+const favNumbers = [1, 3, 7,8,18]
+
+favNumbers.push(100)
+
+favNumbers.unshift()
+
+module.exports = {
+    favFilms
+}
